a4355f0cf192e3748bdf1b24b0cd09edbb47627b
Mark Danovich <mark.danovich@gmail.com>
2017-04-25 18:53:47 +0100

    new host

--- configi.php
+++ configi.php
@@ -4,7 +4,7 @@ session_start();
 
 error_reporting(E_ALL); ini_set('display_errors', 1); 
 
-$mysqli = new mysqli("localhost", "cl26-markd", "oscar87", "cl26-markd");
+$mysqli = new mysqli("host", "user", "pass", "database");
 
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
